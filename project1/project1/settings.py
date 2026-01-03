ebcdde0e229c89f478da2751f5e0d384de49ddaf
NikashDevX <lamsalnikash312@gmail.com>
2026-01-03 12:08:25 +0545

    updated with database

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'project1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mygfgdb',
+        'USER': 'root',
+        'PASSWORD': 'nikk123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+
 
 
 # Password validation
@@ -121,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
